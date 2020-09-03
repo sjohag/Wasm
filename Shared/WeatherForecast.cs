5db475bfc6d3600a50b3f5db04351bdec0e95fb6
Jonas Sjöhag <jonas.sjohag@dekiru.se>
2020-09-03 08:03:39 +0200

    Add project files.

---
+++ Shared/WeatherForecast.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wasm.Shared
+{
+    public interface IWeatherForecast
+    {
+        DateTime Date { get; set; }
+        global::System.String Summary { get; set; }
+        global::System.Int32 TemperatureC { get; set; }
+        global::System.Int32 TemperatureF { get; }
+    }
+
+    public class WeatherForecast : IWeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
